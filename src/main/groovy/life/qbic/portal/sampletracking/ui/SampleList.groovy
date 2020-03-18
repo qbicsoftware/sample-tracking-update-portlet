fab426c20ca4590aeee5b39d246741a6f1faa329
Steffengreiner <Steffen.Greiner@gmx.de>
2020-03-18 08:26:56 +0100

    Created Samplelist Grid with basic refreshList and clearList functionality
    MOVED the removal of samples from Grid  into own Method clearList
    ADD clearButton with clicklistener calling the clearlist Method and notification
    REMOVED caption from Grid
    ADD Label with original caption text
    ADD gridLayout upon which each component is added into
    ADD comments for easier understanding

--- src/main/groovy/life/qbic/portal/sampletracking/ui/SampleList.groovy
+++ src/main/groovy/life/qbic/portal/sampletracking/ui/SampleList.groovy
@@ -1,6 +1,9 @@
 package life.qbic.portal.sampletracking.ui
 
+import com.vaadin.ui.Button
 import com.vaadin.ui.Grid
+import com.vaadin.ui.Label
+import com.vaadin.ui.Notification
 import com.vaadin.ui.VerticalLayout
 import groovy.util.logging.Log4j2
 import life.qbic.datamodel.samples.Sample
@@ -17,12 +20,20 @@ class SampleList extends VerticalLayout{
     }
 
     private def initLayout() {
-        sampleGrid = new Grid("Samples to update:")
+
+        //Define Layout and Label
+        VerticalLayout gridLayout = new VerticalLayout()
+        Label gridLabel = new Label("Samples to update: ")
+
+        //Set Grid properties
         this.sampleGrid.setSelectionMode(Grid.SelectionMode.NONE);
 
         this.sampleGrid.setWidth("100%");
         this.sampleGrid.setHeight("30%");
+
+        //Add data to grid and specify which items should be shown in grid
         try {
+            sampleGrid.setItems(samples); ;
             this.sampleGrid.addColumn({ sample -> sample.getCode() }).setCaption("Id")
             this.sampleGrid.addColumn({ sample -> sample.getCurrentLocation() }).setCaption("Current Location");
             this.sampleGrid.addColumn({sample -> sample.getCurrentLocation().getStatus()}).setCaption("Status")
@@ -30,16 +41,34 @@ class SampleList extends VerticalLayout{
             log.error("Unexpected exception in building the sample Grid", e)
         }
 
+        // Add clear Button to Layout
+        Button clearButton = new Button("Clear samples")
 
-        this.addComponent(this.sampleGrid)
+        // Add clicklistener to clear list upon button activation
+        clearButton.addClickListener({ event ->
+            clearList();
+            Notification.show("Samples removed from list",
+                    Notification.Type.HUMANIZED_MESSAGE)
+        })
+
+        // Add all components to gridlayout
+        gridLayout.addComponent(gridLabel, this.sampleGrid, clearButton);
+
+        // Hand over generated gridlayout
+        this.addComponent(gridLayout)
     }
 
+    // Refresh samples in grid
     void refreshList() {
         if(this.samples) {
             this.sampleGrid.setItems(this.samples)
         } else {
             log.warn("There are no samples to be displayed. Clearing sample grid.")
-            this.sampleGrid.setItems(new ArrayList<Sample>())
+            clearList();
         }
+    }
+    // clear samples in grid
+    void clearList() {
+        this.sampleGrid.setItems(new ArrayList<Sample>())
     }
 }
