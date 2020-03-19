91e1e056ae36ee8cfbceb236094ae8d7363fce36
Steffengreiner <Steffen.Greiner@gmx.de>
2020-03-19 11:19:14 +0100

    Added Vaadin UI Components akin to portal-testing without backend connection
    ADD textfield to show logged in user email
    ADD dropdown menu for sample location selection
    ADD dropdown menu for sample status selection
    ADD date picker menu with current date as default to set sample date
    ADD update sample button
    MOVED clear button component so its instantiated last
    ADD comments for each component

--- src/main/groovy/life/qbic/portal/sampletracking/ui/SampleModifyControls.groovy
+++ src/main/groovy/life/qbic/portal/sampletracking/ui/SampleModifyControls.groovy
@@ -1,13 +1,25 @@
 package life.qbic.portal.sampletracking.ui
 
-import com.vaadin.ui.Button
-import com.vaadin.ui.VerticalLayout
 import life.qbic.portal.sampletracking.app.PortletController
+
+import com.vaadin.ui.Button
+import com.vaadin.ui.DateField
+import com.vaadin.ui.NativeSelect
+import com.vaadin.ui.TextField
+import com.vaadin.ui.VerticalLayout
+
+import java.time.LocalDate
 
 class SampleModifyControls extends VerticalLayout{
 
     private PortletController controller
+    private TextField userEmailField
     private Button clearButton
+    private Button updateSampleButton
+    private NativeSelect<String> locationSelectMenu
+    private NativeSelect<String> sampleSelectMenu
+    private DateField dateSelectMenu
+
 
     SampleModifyControls(PortletController portletController) {
         super()
@@ -17,11 +29,43 @@ class SampleModifyControls extends VerticalLayout{
     }
 
     private def initLayout() {
-        clearButton = new Button("Clear Selection")
+
+        // Add textfield showing email address of responsible Person
+        userEmailField = new TextField("Responsible Person Email")
+        /* For Test purposes
+        userEmailField.setValue("CurrentDev@Qbic.com") */
+        userEmailField.setReadOnly(true)
+        this.addComponent(userEmailField)
+
+        // Add menu allowing location picking for new sample
+        locationSelectMenu = new NativeSelect<>("Select Sample Location")
+        /* For Test purposes
+        locationSelectMenu.setItems("QBiC", "Home Office", "Laboratory", "Shipping") */
+        this.addComponent(locationSelectMenu)
+
+        // Add menu allowing date picking for new sample
+        dateSelectMenu = new DateField("Select Sample Arrival Date")
+        LocalDate localDate = LocalDate.now()
+        dateSelectMenu.setValue(localDate)
+        this.addComponent(dateSelectMenu)
+
+        // Add menu allowing status selection for new sample
+        sampleSelectMenu = new NativeSelect<>("Select Sample Status")
+        /* For Test purposes
+        sampleSelectMenu.setItems("Waiting", "Processing", "Processed") */
+        this.addComponent(sampleSelectMenu)
+
+        // Add button enabling sample update to SampleList
+        updateSampleButton = new Button("Update Sample")
+        this.addComponent(updateSampleButton)
+
+        // Add clear Button to delete samples from SampleList
+        clearButton = new Button("Clear Selected Samples")
         this.addComponent(clearButton)
     }
 
     private void registerListeners() {
         this.clearButton.addClickListener({ event -> controller.clearSelection() })
+
     }
 }
