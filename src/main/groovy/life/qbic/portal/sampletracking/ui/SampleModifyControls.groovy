c2681d466e276f35bb2ed8e0272cf20738ff4d15
KochTobi <tobias.koch@qbic.uni-tuebingen.de>
2020-03-19 13:27:28 +0100

    slight ui modification for integration with view model

--- src/main/groovy/life/qbic/portal/sampletracking/ui/SampleModifyControls.groovy
+++ src/main/groovy/life/qbic/portal/sampletracking/ui/SampleModifyControls.groovy
@@ -1,6 +1,10 @@
 package life.qbic.portal.sampletracking.ui
 
+import com.vaadin.ui.Label
 import groovy.util.logging.Log4j2
+import life.qbic.datamodel.samples.Location
+import life.qbic.datamodel.samples.Sample
+import life.qbic.datamodel.samples.Status
 import life.qbic.portal.sampletracking.app.PortletController
 
 import com.vaadin.ui.Button
@@ -9,18 +13,16 @@ import com.vaadin.ui.NativeSelect
 import com.vaadin.ui.TextField
 import com.vaadin.ui.VerticalLayout
 
-import java.time.LocalDate
-
 @Log4j2
 class SampleModifyControls extends VerticalLayout{
 
     private PortletController controller
-    private TextField userEmailField
+    private Label userEmailField
     private Button clearButton
     private Button updateSampleButton
-    private NativeSelect<String> locationSelectMenu
-    private NativeSelect<String> sampleSelectMenu
-    private DateField dateSelectMenu
+    private NativeSelect<Location> locationSelectMenu
+    private NativeSelect<Status> statusSelectMenu
+    private DateField dateChooser
 
 
     private SampleModifyControls() {
@@ -36,37 +38,47 @@ class SampleModifyControls extends VerticalLayout{
 
     private def initLayout() {
 
-        // Add textfield showing email address of responsible Person
-        userEmailField = new TextField("Responsible Person Email")
+        // Add textfield showing email address of portal user
+        userEmailField = new Label()
+        userEmailField.setValue("You are not logged in.")
         /* For Test purposes
-        userEmailField.setValue("CurrentDev@Qbic.com") */
-        userEmailField.setReadOnly(true)
+        //TODO: remove in release
+        userEmailField.setValue("CurrentDev@Qbic.com")
+        */
         this.addComponent(userEmailField)
 
         // Add menu allowing location picking for new sample
         locationSelectMenu = new NativeSelect<>("Select Sample Location")
+
         /* For Test purposes
-        locationSelectMenu.setItems("QBiC", "Home Office", "Laboratory", "Shipping") */
+        //TODO remove in release
+        locationSelectMenu.setItems("QBiC", "Home Office", "Laboratory", "Shipping")
+        */
         this.addComponent(locationSelectMenu)
 
+
         // Add menu allowing date picking for new sample
-        dateSelectMenu = new DateField("Select Sample Arrival Date")
-        LocalDate localDate = LocalDate.now()
-        dateSelectMenu.setValue(localDate)
-        this.addComponent(dateSelectMenu)
+        dateChooser = new DateField("Desired Arrival Date")
+        dateChooser.setTextFieldEnabled(false)
+        //TODO: choose date format to display
+        this.addComponent(dateChooser)
 
         // Add menu allowing status selection for new sample
-        sampleSelectMenu = new NativeSelect<>("Select Sample Status")
+        statusSelectMenu = new NativeSelect<Status>("Desired Sample Status")
+        statusSelectMenu.setEmptySelectionAllowed(false)
+        statusSelectMenu.setItems(Status.values())
         /* For Test purposes
-        sampleSelectMenu.setItems("Waiting", "Processing", "Processed") */
-        this.addComponent(sampleSelectMenu)
+        //TODO: remove in release
+        sampleSelectMenu.setItems("Waiting", "Processing", "Processed")
+        */
+        this.addComponent(statusSelectMenu)
 
         // Add button enabling sample update to SampleList
-        updateSampleButton = new Button("Update Sample")
+        updateSampleButton = new Button("Update Samples")
         this.addComponent(updateSampleButton)
 
         // Add clear Button to delete samples from SampleList
-        clearButton = new Button("Clear Selected Samples")
+        clearButton = new Button("Clear List")
         this.addComponent(clearButton)
     }
 
