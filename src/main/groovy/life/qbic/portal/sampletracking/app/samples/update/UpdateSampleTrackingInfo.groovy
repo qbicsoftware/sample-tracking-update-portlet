b166143dbc658595e459e67050e287b36a2081a5
KochTobi <KochTobi@users.noreply.github.com>
2020-03-20 10:40:36 +0100

    change comment
    Co-Authored-By: Sven F. <sven.fillinger@mailbox.org>

--- src/main/groovy/life/qbic/portal/sampletracking/app/samples/update/UpdateSampleTrackingInfo.groovy
+++ src/main/groovy/life/qbic/portal/sampletracking/app/samples/update/UpdateSampleTrackingInfo.groovy
@@ -11,7 +11,7 @@ class UpdateSampleTrackingInfo implements SampleTrackingUpdateInput {
     private SampleTrackingUpdateOutput sampleUpdateOutput
 
     private UpdateSampleTrackingInfo() {
-        // prevent default constructor
+        // Prevents accidental call from inside the class
         throw new AssertionError()
     }
 
