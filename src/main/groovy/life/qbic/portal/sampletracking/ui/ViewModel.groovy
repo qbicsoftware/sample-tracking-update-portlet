982f69684421d4431b4722cb1348a0fd8e21d111
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2020-03-30 13:17:46 +0200

    Removes unnused import

--- src/main/groovy/life/qbic/portal/sampletracking/ui/ViewModel.groovy
+++ src/main/groovy/life/qbic/portal/sampletracking/ui/ViewModel.groovy
@@ -3,8 +3,6 @@ package life.qbic.portal.sampletracking.ui
 
 import life.qbic.datamodel.samples.Location
 import life.qbic.datamodel.samples.Sample
-import life.qbic.portal.sampletracking.app.samples.list.ModifySampleListOutput
-import life.qbic.portal.sampletracking.app.samples.query.SampleTrackingQueryOutput
 
 //TODO: rename class to something other than implementation
 class ViewModel {
