82dd3ee8972755333ba67333e5c93ec08a33d190
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2020-04-02 09:49:53 +0200

    Adjusts portlet view with to 100%

--- src/main/groovy/life/qbic/portal/sampletracking/web/views/PortletView.groovy
+++ src/main/groovy/life/qbic/portal/sampletracking/web/views/PortletView.groovy
@@ -24,6 +24,7 @@ class PortletView extends HorizontalLayout {
     }
 
     private def initLayout() {
+        this.setWidth("100%")
         this.addComponents(this.sampleImport, this.sampleList, this.sampleControls)
     }
 }
