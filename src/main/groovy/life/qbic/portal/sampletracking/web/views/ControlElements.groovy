265b870121a2c8c022d6fe988a293bebfeef2754
Steffengreiner <Steffen.Greiner@gmx.de>
2020-04-06 16:55:54 +0200

    Add Forbidden_Status_Option and formatted Code as was suggested in the review

--- src/main/groovy/life/qbic/portal/sampletracking/web/views/ControlElements.groovy
+++ src/main/groovy/life/qbic/portal/sampletracking/web/views/ControlElements.groovy
@@ -10,6 +10,7 @@ import life.qbic.portal.sampletracking.web.ViewModel
 @Log4j2
 class ControlElements extends VerticalLayout {
 
+    final static List<Status> FORBIDDEN_STATUS_OPTIONS = new ArrayList([Status.DATA_AT_QBIC, Status.METADATA_REGISTERED])
 
     final private PortletController controller
     final private ViewModel viewModel
@@ -48,10 +49,12 @@ class ControlElements extends VerticalLayout {
         // Add menu allowing status selection for new sample
         statusSelectMenu = new NativeSelect<Status>("New Sample Status")
         statusSelectMenu.setEmptySelectionAllowed(false)
-List<Status> selectableStatusOptions = Status.values().findall { status ->
-  !(status in FORBIDDEN_STATUS_OPTIONS)
-}
-statusSelectMenu.setItems(selectableStatusOptions)
+
+        List<Status> selectableStatusOptions = Status.values().findAll { status ->
+            !(status in FORBIDDEN_STATUS_OPTIONS)
+        }
+
+        statusSelectMenu.setItems(selectableStatusOptions)
 
         // Add button enabling sample update to SampleList
         updateSampleButton = new Button("Update Samples")
