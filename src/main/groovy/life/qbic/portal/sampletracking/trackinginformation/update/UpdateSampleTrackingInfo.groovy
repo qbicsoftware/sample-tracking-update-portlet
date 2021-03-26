374d9eea2c8200c8de75cf3cee8b071dcec105f6
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2021-03-26 10:12:29 +0100

    Update interfaces for update multiple

--- src/main/groovy/life/qbic/portal/sampletracking/trackinginformation/update/UpdateSampleTrackingInfo.groovy
+++ src/main/groovy/life/qbic/portal/sampletracking/trackinginformation/update/UpdateSampleTrackingInfo.groovy
@@ -10,12 +10,6 @@ class UpdateSampleTrackingInfo implements SampleTrackingUpdateInput {
     final SampleTrackingUpdateDataSource sampleTrackingUpdateDataSource
     private SampleTrackingUpdateOutput sampleUpdateOutput
 
-    private UpdateSampleTrackingInfo() {
-        // Prevents accidental call from inside the class
-        throw new AssertionError()
-    }
-
-
     UpdateSampleTrackingInfo(SampleTrackingUpdateDataSource sampleTrackingUpdateDataSource, SampleTrackingUpdateOutput sampleUpdateOutput) {
         this.sampleTrackingUpdateDataSource = sampleTrackingUpdateDataSource
         this.sampleUpdateOutput = sampleUpdateOutput
@@ -35,12 +29,35 @@ class UpdateSampleTrackingInfo implements SampleTrackingUpdateInput {
     @Override
     def setCurrentSampleLocation(String sampleId, Location location) {
         try {
-            this.sampleTrackingUpdateDataSource.updateSampleLocation(sampleId, location)
+            updateSampleLocation(sampleId, location)
             sampleUpdateOutput.updateFinished(sampleId)
         } catch (SampleTrackingUpdateException e) {
             log.error e
             this.sampleUpdateOutput.invokeOnError "Could not update location for sample $sampleId."
 
         }
+    }
+
+    /**
+     * This method shall be called, when multiple samples and their current locations are updated
+     * @param updateInformation a map containing sample codes as keys and the desired location as value
+     * @since 1.3.0
+     */
+    @Override
+    void updateMultipleSampleLocations(Map<String, Location> updateInformation) {
+        //TODO implement
+    }
+
+
+    /**
+     *
+     * @param sampleId the sample identifier
+     * @param location the new location the sample identifier should be set to
+     * @throws SampleTrackingUpdateException forwards exceptions directly from the DataSource
+     * @see life.qbic.portal.sampletracking.trackinginformation.update.SampleTrackingUpdateDataSource#updateSampleLocation(java.lang.String, life.qbic.datamodel.samples.Location)
+     * @since 1.3.0
+     */
+    private void updateSampleLocation(String sampleId, Location location) throws SampleTrackingUpdateException {
+        this.sampleTrackingUpdateDataSource.updateSampleLocation(sampleId, location)
     }
 }
